25abd1b417d7a464b226625d51343ced82ef0d37
Paulo Henrique <paulo.ricklima788@gmail.com>
2026-05-17 17:47:41 -0300

    models.py

--- ApiSoap/models.py
+++ ApiSoap/models.py
@@ -1,7 +1,11 @@
 from spyne import Application, rpc, ServiceBase, Integer, Unicode, Iterable, ComplexModel
 from spyne.protocol.soap import Soap11
 
-
+from spyne import (
+    Application, rpc, ServiceBase,
+    Integer, Unicode, Iterable, ComplexModel,
+    Decimal, Date
+)
 
 
 class Fornecedor(ComplexModel):
@@ -20,7 +24,7 @@ class Cliente(ComplexModel):
     id_cliente = Integer
     nome = Unicode
     cidade = Unicode
-    data_cadastro = Unicode
+    data_cadastro = Date
     tipo_cliente = Unicode
 
 
@@ -28,30 +32,30 @@ class Vendedor(ComplexModel):
     id_vendedor = Integer
     nome = Unicode
     regiao = Unicode
-    salario = Unicode
-    data_admissao = Unicode
+    salario = Decimal
+    data_admissao = Date
 
 
 class Produto(ComplexModel):
     id_produto = Integer
     nome_produto = Unicode
-    preco = Unicode
-    custo = Unicode
+    preco = Decimal
+    custo = Decimal
     id_categoria = Integer
     id_fornecedor = Integer
 
 
 class Estoque(ComplexModel):
     id_estoque = Integer
-    quantidade = Unicode
-    estoque_minimo = Unicode
-    data_atualizacao = Unicode
+    quantidade = Decimal
+    estoque_minimo = Decimal
+    data_atualizacao = Date
     id_produto = Integer
 
 
 class Pedido(ComplexModel):
     id_pedido = Integer
-    data_pedido = Unicode
+    data_pedido = Date
     status_pedido = Unicode
     id_cliente = Integer
     id_vendedor = Integer
@@ -59,7 +63,7 @@ class Pedido(ComplexModel):
 
 class ItemPedido(ComplexModel):
     id_item = Integer
-    quantidade = Unicode
-    valor_unitario = Unicode
+    quantidade = Decimal
+    valor_unitario = Decimal
     id_pedido = Integer
     id_produto = Integer
